8320f079918e8ac1ad5d13b68af0ffdb613ead4a
KEWANG <kwang655@usc.edu>
2023-10-13 16:00:39 -0700

    8.3.material

--- src/shader.cpp
+++ src/shader.cpp
@@ -106,3 +106,13 @@ void Shader::setMat4(const std::string& name, glm::mat4 &value) const
     
     glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
 }
+
+void Shader::setVec3(const std::string& name, glm::vec3 &value) const
+{
+    glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+void Shader::setVec3(const std::string& name, float x, float y, float z) const
+{
+    glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+}
