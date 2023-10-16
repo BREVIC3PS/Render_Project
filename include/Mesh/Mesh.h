21b732a74246c6389f007ea6b071dc19dc90d695
KEWANG <kwang655@usc.edu>
2023-10-15 21:56:14 -0700

    First Model!

---
+++ include/Mesh/Mesh.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <vector>
+#include <string>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+    aiString path;
+};
+
+class Shader;
+
+class Mesh {
+public:
+    /*  网格数据  */
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+    /*  函数  */
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+    void Draw(Shader shader);
+private:
+    /*  渲染数据  */
+    unsigned int VAO, VBO, EBO;
+    /*  函数  */
+    void setupMesh();
+};
---
+++ include/Mesh/Model.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Mesh/Mesh.h"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+unsigned int TextureFromFile(const char* path, const std::string& directory, bool gamma = false);
+
+class Model
+{
+public:
+    /*  函数   */
+    Model(const char* path)
+    {
+        loadModel(path);
+    }
+    void Draw(Shader shader);
+private:
+    /*  模型数据  */
+    std::vector<Mesh> meshes;
+    std::string directory;
+    /*  函数   */
+    void loadModel(std::string path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type,
+        std::string typeName);
+};
